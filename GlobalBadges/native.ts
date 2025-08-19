268ff9c8df34ffd069434a2b178a7ca42b26a29c
ScribblrBot <scribblrbot@gmail.com>
2025-08-19 14:57:21 -0400

    Delete GlobalBadges/native.ts

--- GlobalBadges/native.ts
+++
@@ -1,4 +0,0 @@
-import { CspPolicies } from "@main/csp";
-
-CspPolicies["api.domi-btnr.dev"] = [...(CspPolicies["api.domi-btnr.dev"] ?? []), "connect-src", "img-src"];
-CspPolicies["gb.obamabot.me"] = [...(CspPolicies["gb.obamabot.me"] ?? []), "img-src"];
